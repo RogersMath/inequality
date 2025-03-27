add9d64e45d4c2f6f8b07326a2c18fb61efb701b
RogersMath <116944378+RogersMath@users.noreply.github.com>
2025-03-27 13:48:57 +0000

    fixing for gh pages

--- inequality/vite.config.js
+++ inequality/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // For GitHub Pages deployment
+  base: '/inequality/', // This should match your repository name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
